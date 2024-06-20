4760f3d2eb0ff5511c22de5d64ab554a1c400174
divijsehgal <137478155+divijsehgal@users.noreply.github.com>
2024-06-20 16:24:48 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+	  backend "s3" {
+    bucket = "fg-bucket-03"
+    key    = "dyuti/awstest/terraform.tfstate"
+  }
+}
+
+provider "aws" {
+region = "us-west-1"
+}
+
+resource "aws_s3_bucket" "fd_bucket" {
+  bucket = "fd-018-bucket"  # Replace with your desired bucket name
+  acl    = "private"
+  }
+
+output "s3_bucket_id" {
+  description = "The name of the bucket."
+  value       = aws_s3_bucket.fd_bucket.id
+}
