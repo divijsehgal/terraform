79ae46dbe81b9c4a4995405957c0ec8ddb8dcd4d
divijsehgal <137478155+divijsehgal@users.noreply.github.com>
2024-06-22 15:59:49 +0530

    Create main.tf

---
+++ AzureDevop/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azuredevops = {
+      source="microsoft/azuredevops"
+      version = ">=0.1.0"
+    }
+  }
+}
+
+resource "azuredevops_project" "project" {
+  name        = "ProjectFDTF"
+  description = "Project From FlexDeploy"
+}
