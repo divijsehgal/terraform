7a747f131bd46c28ff288a69ec2752e64f54f540
divijsehgal <137478155+divijsehgal@users.noreply.github.com>
2024-06-27 12:05:34 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     key="terraformStateFile/terraform.tfstate"
-    bucket = "flexdeploy-test-bucket"
+    bucket = "flexdeploy-test-bucket2"
     region = "us-east-1"
   }
 }
